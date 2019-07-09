17a54d803b7195b546830fc8220fb349812af9ec
Hichem AMAR BENSABER <amar-bensaber.h@sfeir.com>
2019-07-09 11:45:47 +0200

    feats: nodes + links training

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 let $ = go.GraphObject.make;
 let myDiagram =
-   $(go.Diagram, "myDiagramDiv");
+   $(go.Diagram, 'myDiagramDiv');
 
-let myModel = $(go.Model);
+
+myDiagram.nodeTemplate =
+   $(go.Node, 'Horizontal',
+
+      {background: '#DE6816'},
+
+      $(go.TextBlock,
+         'Default Text',
+         {margin: 12, stroke: '#FFFFFF', font: 'bold 16px sans-serif'},
+
+         new go.Binding('text', 'name'))
+   );
+
+
+let myModel = $(go.TreeModel);
 
 myModel.nodeDataArray = [
-   {key: 'Hichem'},
-   {key: 'Momo'},
-   {key: 'Farah'},
+   {key: '1', name: 'A'},
+   {key: '2', parent: '1', name: 'B'},
+   {key: '3', parent: '2', name: 'C'},
+   {key: '4', parent: '3', name: 'D'},
 ];
 
 myDiagram.model = myModel;
