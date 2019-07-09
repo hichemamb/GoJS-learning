9a14c9212c2392a5cace49eb991ad9ef15a47958
Hichem AMAR BENSABER <amar-bensaber.h@sfeir.com>
2019-07-09 11:12:55 +0200

    feats: Created diagram and first  model

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert('gojs');
+let $ = go.GraphObject.make;
+let myDiagram =
+   $(go.Diagram, "myDiagramDiv");
+
+let myModel = $(go.Model);
+
+myModel.nodeDataArray = [
+   {key: 'Hichem'},
+   {key: 'Momo'},
+   {key: 'Farah'},
+];
+
+myDiagram.model = myModel;
